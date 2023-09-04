a54184ba9fbfb4d06d76ffdf13963ecff84b9860
santoshvishwakarma27 <vishwakarma0011@gmail.com>
2023-09-04 20:41:38 +0530

    First Commit 04092023_1

---
+++ src/test/java/testPackage/Sel2.java
@@ -0,0 +1,22 @@
+package testPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sel2 
+{
+	@Test
+	public void execute()
+	{
+		
+	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.co.in/");
+	driver.quit();
+	//Dimension d=new Dimension(400,900);
+	//driver.manage().window().setSize(d);
+	}
+
+
+}
